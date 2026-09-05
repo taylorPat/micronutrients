e53980ac2106dcf242a7eaaa2b8a085aa04e998c
taylorPat <patrick.schneider9@web.de>
2026-09-05 22:25:47 +0200

    feat: run db service inside dockerfile

--- src/services/db/engine.py
+++ src/services/db/engine.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL_FROM_LOCAL = "postgresql+psycopg://user:pw@localhost:5432/pdb"
 
-DATABASE_URL = DATABASE_URL_FROM_LOCAL
+DATABASE_URL = os.getenv("DATABASE_URL") or DATABASE_URL_FROM_LOCAL
 
 engine = create_engine(url=DATABASE_URL)
 
